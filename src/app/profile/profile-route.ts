4087dc020afdfa4478f99968075d1668977dfff3
muhammadfikri4 <muhfikriantoaji@gmail.com>
2024-10-26 19:20:06 +0700

    fix: add validate request

--- src/app/profile/profile-route.ts
+++ src/app/profile/profile-route.ts
@@ -4,11 +4,13 @@ import {
   getProfileController,
 } from "./profile-controller";
 import { VerifyUser } from "../../middleware/verifyToken";
+import { validateRequest } from "middleware/validateRequest";
+import { updatePasswordSchema } from "./profile-request";
 
 const route = Router();
 
 route.get("/", VerifyUser, getProfileController);
 // route.patch("/password", VerifyUser, changePasswordController);
-route.patch("/forgot-password", changePasswordController);
+route.patch("/forgot-password", validateRequest(updatePasswordSchema) ,changePasswordController);
 
 export default route;
